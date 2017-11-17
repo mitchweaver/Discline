ee1bb21a7b5a49b31cac8700027f48d16a46a9be
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-11-18 00:46:51 +0100

    add printutils

---
+++ printutils.py
@@ -0,0 +1,20 @@
+import discord
+
+def lineBreak():
+    print('---------------------------------')
+
+
+def printServers(client):
+    tmp = ""
+    for server in client.servers:
+        tmp += server.name
+    print("Available servers: " + tmp)
+
+def printUser(client):
+    print('Logged in as: ' + client.user.name)
+
+def printChannels(server):
+    print("Available channels:")
+    lineBreak();
+    for channel in  server.channels:
+        print(channel.name)
