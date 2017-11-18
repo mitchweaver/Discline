2b0320d7fdd76251a65c864c887813ead6f96de2
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-11-18 04:14:51 +0100

    goalpost: message sending
    goalpost: message sending

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import curses
 import asyncio
 import sys
 from printutils import *
+from client import client
 
 # await client.login('zemajujo@axsup.net', 'testpassword')
 
 token = sys.argv[1]
-client = discord.Client(max_messages=10000)
+client = client(max_messages=100)
 servers = []
 prefix = '/'
 
-current_server=""
-current_channel=""
+currentServer="DisKvlt"
+currentChannel="terminal_discord"
+prompt="[ ~ ]"
 
 ####### INITIALIZATION ################################################
 def initServers():
@@ -22,21 +24,25 @@ def initServers():
         servers.append(server)
 #######################################################################
 
-@client.event
+@client.async_event
 async def on_ready(): 
     print("Client is starting...")
-    print("Client has logged in successfully!")
+    client.wait_until_login()
+    if client.is_logged_in:
+        print("Client has logged in successfully!")
+    else:
+        print("Error: Login failed")
+        quit()
     print("Initializing...")
     initServers()
     lineBreak()
     printUser(client)
     printServers(client)
     lineBreak()
-    printChannels(servers[0])
-    lineBreak()
 
-    
-    
-
-
+    while True:
+        input = input(prompt)
+        # await client.send_message(client.getChannel("terminal_discord"), a)
+                              
+                              
 client.run(token, bot=False)
