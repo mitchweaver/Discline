ba642538ad7f70b7c8e43b1ef3e2fa67fa8f8578
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-12-05 04:02:09 +0100

    add italic and bold font supportc

--- on_message.py
+++ on_message.py
@@ -1,19 +1,70 @@
 from ui import print_screen
 from globals import *
 from settings import *
+import re
+
+async def trim_emoji(full_name, short_name, string):
+    return string.replace(full_name, ":" + short_name + ":")
+
+async def convert_bold(string):
+    sections = string.split("**")
+    left = sections[0]
+    target = sections[1]
+    right = "".join(sections[2])
+    return term.normal + left + " " + term.bold(target) + term.normal + " " + right
+
+async def convert_italic(string):
+    sections = string.split("*")
+    left = sections[0]
+    target = sections[1]
+    right = "".join(sections[2])
+    return term.normal + left + " " + term.italic(target) + term.normal + " " + right
 
 async def calc_mutations(msg):
-    text = ""
 
     # if the message is a file, extract the discord url from it
     try:
         json = str(msg.attachments[0]).split("'")
-        text = json[5]
-        msg.content = text
+        msg.content = json[5]
         return msg
+    
+    # # otherwise it must not have any attachments and its a regular message
     except:
-        return msg
+        text = msg.content
 
+        # check to see if it has any custom-emojis
+        # These will look like <:emojiname:39432432903201>
+        # We will recursively trim this into just :emojiname:
+        if msg.server.emojis is not None and len(msg.server.emojis) > 0:
+            for emoji in msg.server.emojis:
+                full_name = "<:" + emoji.name + ":" + emoji.id + ">" 
+                                    
+                while full_name in text:
+                    text = await trim_emoji(full_name, emoji.name, text)
+
+            msg.content = text
+
+        # check for boldened font
+        if text.count("**") > 1:
+            while("**") in text:
+                text = await convert_bold(text)            
+
+            msg.content = text
+
+        # check for italic font
+        if text.count("*") > 1:
+            while("*") in text:
+                text = await convert_italic(text)            
+
+            msg.content = text
+
+
+        
+        # check for italicized font
+        
+
+
+        return msg
 
 
 
