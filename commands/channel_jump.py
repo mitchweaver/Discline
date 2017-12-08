84c5962177b7e4ca4be7e42790d1758b6d13f62b
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-12-08 05:17:49 +0100

    add channel jump command (/c1, /c2, etc)

---
+++ commands/channel_jump.py
@@ -0,0 +1,26 @@
+from utils.globals import server_log_tree, client
+from utils.quicksort import quick_sort_channel_logs
+from settings import ARRAYS_START_AT_ZERO
+
+async def channel_jump(arg):
+    logs = []
+
+    num = int(arg[1:]) - 1
+ 
+    # sub one to allow for "/c0" being the top channel
+    if ARRAYS_START_AT_ZERO:
+        num -= 1
+   
+    # in case someone tries to go to a negative index
+    if num <= -1:
+        num = 0
+
+    for slog in server_log_tree:
+        if slog.get_server() is client.get_current_server():
+            for clog in slog.get_logs():
+                logs.append(clog)
+
+    logs = quick_sort_channel_logs(logs)
+
+
+    client.set_current_channel(logs[num].get_name()) 
