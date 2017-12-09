b8f55103b8d94b959c01fba6057e631bd2d72da7
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-12-09 08:27:09 +0100

    externalize msg manipulation functions

---
+++ ui/text_manipulation.py
@@ -0,0 +1,141 @@
+import re
+from discord import MessageType
+from settings import *
+from utils.globals import *
+
+async def calc_mutations(msg):
+
+    # if the message is a file, extract the discord url from it
+    try:
+        json = str(msg.attachments[0]).split("'")
+        msg.content = json[5]
+        return msg
+    
+    # # otherwise it must not have any attachments and its a regular message
+    except:
+        text = msg.content
+
+
+        # check for in-line code blocks
+        if text.count("```") > 1:
+            while("```") in text:
+                text = await convert_code_block(text)
+
+            msg.content = text
+
+            # TODO: if there are asterics or __'s in the code, then
+            # this will not stop them from being formatted
+
+
+        # check for in-line code marks
+        if text.count("`") > 1:
+            while("`") in text:
+                text = await convert_code(text)
+
+            msg.content = text
+
+        # check to see if it has any custom-emojis
+        # These will look like <:emojiname:39432432903201>
+        # We will recursively trim this into just :emojiname:
+        if msg.server.emojis is not None and len(msg.server.emojis) > 0:
+            for emoji in msg.server.emojis:
+                full_name = "<:" + emoji.name + ":" + emoji.id + ">" 
+                                    
+                while full_name in text:
+                    text = await trim_emoji(full_name, emoji.name, text)
+
+            msg.content = text
+
+        # check for boldened font
+        if text.count("**") > 1:
+            while("**") in text:
+                text = await convert_bold(text)            
+
+            msg.content = text
+
+        # check for italic font
+        if text.count("*") > 1:
+            while("*") in text:
+                text = await convert_italic(text)            
+
+            msg.content = text
+
+        # check for underlined font 
+        if text.count("__") > 1:
+            while("__") in text:
+                text = await convert_underline(text)            
+
+            msg.content = text
+
+        # check for urls
+        if "http://" in text or "https://" in text or "www." in text \
+           or "ftp://" in text or ".com" in text:
+
+            msg.content = await convert_url(text)
+
+        # else it must be a regular message, nothing else
+        return msg
+
+
+
+async def trim_emoji(full_name, short_name, string):
+    return string.replace(full_name, ":" + short_name + ":")
+
+async def convert_bold(string):
+    sections = string.split("**")
+    left = sections[0]
+    target = sections[1]
+    right = "".join(sections[2])
+    return term.normal + term.white + left + " " + term.bold(target) + term.normal + \
+            term.white + " " + right
+
+async def convert_italic(string):
+    sections = string.split("*")
+    left = sections[0]
+    target = sections[1]
+    right = "".join(sections[2])
+    return term.normal + term.white +  left + " " + term.italic(target) + term.normal + \
+            term.white + " " + right
+
+async def convert_underline(string):
+    sections = string.split("__")
+    left = sections[0]
+    target = sections[1]
+    right = "".join(sections[2])
+    return term.normal + term.white + left + " " + term.underline(target) + term.normal + \
+            term.white + " " + right
+
+async def convert_code(string):
+    sections = string.split("`")
+    left = sections[0]
+    target = sections[1]
+    right = "".join(sections[2])
+    return term.normal + term.white +  left + " " + await get_color(CODE_BLOCK_COLOR) \
+            + target  + term.normal \
+            + term.white + " " + right
+
+async def convert_code_block(string):
+    sections = string.split("```")
+    left = sections[0]
+    target = sections[1]
+    right = "".join(sections[2])
+    return term.normal + term.white +  left + " " + term.on_black(target) + term.normal + \
+            term.white + " " + right
+
+async def convert_url(string):
+    formatted_line = []
+    entities = []
+    if " " in string:
+        entities = string.split(" ")
+    else:
+        entities.append(string)
+
+    for entity in entities:
+        if "http://" in entity or "https://" in entity or "www." in entity \
+           or "ftp://" in entity or ".com" in entity:
+            entity = await get_color(URL_COLOR) + term.italic + term.underline + entity + term.normal
+        formatted_line.append(entity)
+
+    return " ".join(formatted_line)
+
+
