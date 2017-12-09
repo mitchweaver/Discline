5d4bbdc1294b943d756d732126d27cf4094aed80
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-12-09 09:10:32 +0100

    remove unecessary try/catch

--- client/on_message.py
+++ client/on_message.py
@@ -8,17 +8,15 @@ async def on_incoming_message(msg):
     # TODO: make sure it isn't a private message
     
     # find the server/channel it belongs to and add it
-    try: # (note: the try/except here is to be able to break out of the double for loop)
-        for server_log in server_log_tree:
-            if server_log.get_server() == msg.server:
-                for channel_log in server_log.get_logs():
-                    if channel_log.get_channel() == msg.channel:
-                        
-                        channel_log.append(await calc_mutations(msg))
-                        
-                        if channel_log.get_channel() is not client.get_current_channel():
-                            channel_log.unread = True
-                        raise Found
-    except:
-        # redraw the screen
-        await print_screen()
+    for server_log in server_log_tree:
+        if server_log.get_server() == msg.server:
+            for channel_log in server_log.get_logs():
+                if channel_log.get_channel() == msg.channel:
+                    
+                    channel_log.append(await calc_mutations(msg))
+                    
+                    if channel_log.get_channel() is not client.get_current_channel():
+                        channel_log.unread = True
+
+    # redraw the screen
+    await print_screen()
