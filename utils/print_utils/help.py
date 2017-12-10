fcd656a48e1238a891aafc1812ab0bc1eaed8d1f
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-12-10 19:15:33 +0100

    update help

--- utils/print_utils/help.py
+++ utils/print_utils/help.py
@@ -8,6 +8,7 @@ async def print_help():
         + "--------------------------------------------- \n" \
         + get_line("/channel", "   - ", "switch to channel - (alias: 'c')") \
         + get_line("/server", "    - ", "switch server     - (alias: 's')") \
+        + term.cyan + "note: these commands can now fuzzy-find!" \
         + "\n" \
         + get_line("/servers", "   - ", "list available servers") \
         + get_line("/channels", "  - ", "list available channels") \
@@ -21,12 +22,13 @@ async def print_help():
         + term.cyan + "This can be configured to start at 0 in settings.py" \
         + "\n" \
         + "\n" \
-        + get_line("clear", "      - ", "force clearing of screen") \
+        + get_line("/clear", "      - ", "force clearing of screen") \
         + get_line("/quit", "      - ", "exit cleanly") \
         + "\n \n" \
         + term.magenta + "Note: You can send emojis by using :emojiname: \n" \
         + term.cyan + "Nitro emojis do work! Make sure you have \n" \
         + term.cyan + "nitro enabled in your settings. \n" \
+        + "\n"
         + term.yellow + "You can scroll up/down in channel logs \n" \
         + term.yellow + "by using PageUp/PageDown." \
         + term.green + "~ \n" \
