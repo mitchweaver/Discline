1b12df6dcccdcff65c88a66b6b1a8bc12dabadd4
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-12-11 09:32:24 +0100

    update help

--- utils/print_utils/help.py
+++ utils/print_utils/help.py
@@ -1,10 +1,18 @@
 from os import system
 from utils.globals import term
 
-async def print_help():
+def print_help():
     system("clear")
-    system("echo '" + term.green \
-        + "Available Commands: \n" + term.red \
+    system("echo '" + term.normal \
+        + term.green("Launch Arguments: \n") + term.red \
+        + "--------------------------------------------- \n" \
+        + get_line("--copy-skeleton ", " --- ", "copies template settings") \
+        + term.cyan("This file can be found at ~/.config/Discline/config \n") \
+        + "\n"
+        + get_line("--store-token", " --- ", "stores your token") \
+        + term.cyan("This file can be found at ~/.config/Discline/token \n") \
+        + "\n"
+        + term.green("Available Commands: \n") + term.red \
         + "--------------------------------------------- \n" \
         + get_line("/channel", "   - ", "switch to channel - (alias: 'c')") \
         + get_line("/server", "    - ", "switch server     - (alias: 's')") \
@@ -19,17 +27,17 @@ async def print_help():
         + get_line("/file", "      - ", "upload a file via path") \
         + "\n" \
         + get_line("/cX", "        - ", "shorthand to change channel (Ex: /c1)") \
-        + term.cyan + "This can be configured to start at 0 in settings.py" \
+        + term.cyan("This can be configured to start at 0 in settings.py") \
         + "\n" \
         + "\n" \
         + get_line("/quit", "      - ", "exit cleanly") \
         + "\n \n" \
         + term.magenta + "Note: You can send emojis by using :emojiname: \n" \
-        + term.cyan + "Nitro emojis do work! Make sure you have \n" \
-        + term.cyan + "nitro enabled in your settings. \n" \
+        + term.cyan("Nitro emojis do work! Make sure you have \n") \
+        + term.cyan("nitro enabled in your settings. \n") \
         + "\n"
         + term.yellow + "You can scroll up/down in channel logs \n" \
-        + term.yellow + "by using PageUp/PageDown." \
+        + term.yellow + "by using PageUp/PageDown. \n" \
         + term.green + "~ \n" \
         + term.green + "~ \n" \
         + term.green + "~ \n" \
@@ -39,4 +47,4 @@ async def print_help():
 
 
 def get_line(command, div, desc):
-    return term.yellow + command + term.cyan + div + term.normal + desc + "\n"
+    return term.yellow(command) + term.cyan(div) + term.normal + desc + "\n"
