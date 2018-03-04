76d5e71500a8388438c86cffff8108c6e955d578
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2018-03-04 13:38:40 -0300

    Fix printing of multiple bold snippets on one message
    Ran a bit of flake8 compliance correction as well.
    So far the line breaks after the second bold snippet,
    for some weird reason. Will investigate.

--- ui/text_manipulation.py
+++ ui/text_manipulation.py
@@ -1,12 +1,11 @@
-import re
 from discord import MessageType
 from utils.settings import settings
 from utils.globals import gc, get_color
-import utils
+
 
 async def calc_mutations(msg):
 
-    try: # if the message is a file, extract the discord url from it
+    try:  # if the message is a file, extract the discord url from it
         json = str(msg.attachments[0]).split("'")
         for string in json:
             if string is not None and string != "":
@@ -14,10 +13,9 @@ async def calc_mutations(msg):
                     msg.content = string
                     break
     except IndexError: pass
-    
+
     # otherwise it must not have any attachments and its a regular message
     text = msg.content
-
 
     # check for in-line code blocks
     if text.count("```") > 1:
@@ -40,8 +38,8 @@ async def calc_mutations(msg):
     # We will recursively trim this into just :emojiname:
     if msg.server.emojis is not None and len(msg.server.emojis) > 0:
         for emoji in msg.server.emojis:
-            full_name = "<:" + emoji.name + ":" + emoji.id + ">" 
-                                
+            full_name = "<:" + emoji.name + ":" + emoji.id + ">"
+
             while full_name in text:
                 text = await trim_emoji(full_name, emoji.name, text)
 
@@ -50,27 +48,27 @@ async def calc_mutations(msg):
     # check for boldened font
     if text.count("**") > 1:
         while("**") in text:
-            text = await convert_bold(text)            
+            text = await convert_bold(text)
 
         msg.content = text
 
     # check for italic font
     if text.count("*") > 1:
         while("*") in text:
-            text = await convert_italic(text)            
+            text = await convert_italic(text)
 
         msg.content = text
 
-    # check for underlined font 
+    # check for underlined font
     if text.count("__") > 1:
         while("__") in text:
-            text = await convert_underline(text)            
+            text = await convert_underline(text)
 
         msg.content = text
 
     # check for urls
-    if "http://" in text or "https://" in text or "www." in text \
-        or "ftp://" in text or ".com" in text:
+    if "http://" in text or "https://" in text or "www." in text or \
+       "ftp://" in text or ".com" in text:
 
         msg.content = await convert_url(text)
 
@@ -80,6 +78,7 @@ async def calc_mutations(msg):
 
     # else it must be a regular message, nothing else
     return msg
+
 
 async def convert_pin(msg):
     name = ""
@@ -92,21 +91,25 @@ async def convert_pin(msg):
 async def trim_emoji(full_name, short_name, string):
     return string.replace(full_name, ":" + short_name + ":")
 
+
 async def convert_bold(string):
     sections = string.split("**")
-    left = sections[0]
-    target = sections[1]
-    right = "".join(sections[2])
-    return gc.term.normal + gc.term.white + left + " " + gc.term.bold(target) + gc.term.normal + \
-            gc.term.white + " " + right
+    ret = ""
+    for normal, bold in zip(sections[::2], sections[1::2]):
+        # Iterate over pairs. The trick is that the second one is always bold
+        ret += gc.term.normal + gc.term.white + normal + \
+               gc.term.bold(bold) + gc.term.normal + gc.term.white
+    return ret
+
 
 async def convert_italic(string):
     sections = string.split("*")
     left = sections[0]
     target = sections[1]
     right = "".join(sections[2])
-    return gc.term.normal + gc.term.white +  left + " " + gc.term.italic(target) + gc.term.normal + \
-            gc.term.white + " " + right
+    return gc.term.normal + gc.term.white + left + " " + gc.term.italic(target) + gc.term.normal + \
+        gc.term.white + " " + right
+
 
 async def convert_underline(string):
     sections = string.split("__")
@@ -114,7 +117,8 @@ async def convert_underline(string):
     target = sections[1]
     right = "".join(sections[2])
     return gc.term.normal + gc.term.white + left + " " + gc.term.underline(target) + gc.term.normal + \
-            gc.term.white + " " + right
+        gc.term.white + " " + right
+
 
 async def convert_code(string):
     sections = string.split("`")
@@ -122,8 +126,9 @@ async def convert_code(string):
     target = sections[1]
     right = "".join(sections[2])
     return gc.term.normal + gc.term.white +  left + " " + await get_color(settings["code_block_color"]) \
-            + target  + gc.term.normal \
+            + target + gc.term.normal \
             + gc.term.white + " " + right
+
 
 async def convert_code_block(string):
     sections = string.split("```")
@@ -131,7 +136,8 @@ async def convert_code_block(string):
     target = sections[1]
     right = "".join(sections[2])
     return gc.term.normal + gc.term.white +  left + " " + gc.term.on_black(target) + gc.term.normal + \
-            gc.term.white + " " + right
+        gc.term.white + " " + right
+
 
 async def convert_url(string):
     formatted_line = []
@@ -148,5 +154,3 @@ async def convert_url(string):
         formatted_line.append(entity)
 
     return " ".join(formatted_line)
-
-
